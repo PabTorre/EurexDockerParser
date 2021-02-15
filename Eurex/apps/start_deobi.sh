b0d79397a39ad9ed899fd3d9e2eb248a6af69b2f
Pablo Torre <xctico@gmail.com>
2021-02-14 19:05:07 -0500

    map to local volume /c/data/input/

--- Eurex/apps/start_deobi.sh
+++ Eurex/apps/start_deobi.sh
@@ -1,1 +1,1 @@
-./deobi -s eobi.xml -c output.conf --input-file ../data/input/* --output-directory ../data/output/
+./deobi -s eobi.xml -c output.conf --input-file ./data/input/* --output-directory ./data/output/
