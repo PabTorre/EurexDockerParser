bce6c892ea0f588c53807ecabac411d7999e4683
Jan Pospíšil <honik6@gmail.com>
2021-03-19 11:23:45 +0100

    updated to use xml 8.0

--- Eurex/apps/start_deobi.sh
+++ Eurex/apps/start_deobi.sh
@@ -1,1 +1,3 @@
-./deobi -s eobi.xml -c output.conf --input-file ./data/input/* --output-directory ./data/output/
+# ./deobi -s eobi.xml -c output.conf --input-file ./data/input/* --output-directory ./data/output/
+
+./deobi -s EOBI-R80-for-deobi.xml -c output.conf --input-file ./data/input/2350*.pcap --output-directory ./data/output/
